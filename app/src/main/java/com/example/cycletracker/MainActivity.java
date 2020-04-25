7118203c18273c56eff9dc5e58379b6973938396
Andrew Chen <andrew.chen.us@gmail.com>
2020-04-25 15:42:28 -0700

    -Added a imperfect number randomizer

---
+++ app/src/main/java/com/example/cycletracker/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.cycletracker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        final TextView resultsTextView = findViewById(R.id.resultTextView);
+        final SeekBar seekBar = findViewById(R.id.seekBar);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random r = new Random();
+                int random = r.nextInt(seekBar.getProgress());
+                resultsTextView.setText("" + random);
+            }
+        });
+    }
+}
